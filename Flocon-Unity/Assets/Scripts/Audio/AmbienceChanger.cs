bb1486bc8562acef05b6c5fbb17ccc72cf246d44
guerric <guerric.chab@gmail.com>
2020-10-06 14:36:40 +0200

    Audio tool for amb / event trigger added

---
+++ Flocon-Unity/Assets/Scripts/Audio/AmbienceChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbienceChanger : MonoBehaviour
+{
+    [Header("Parameter")]
+    public bool m_changeParameter = true;
+    public string m_parameterName;
+    public float m_parameterValue;
+
+    [Header("Event Trigger")]
+    public bool m_triggerEvent = false;
+    public string m_eventName = "event:/Example";
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            if(m_changeParameter)
+            {
+                FMODUnity.RuntimeManager.StudioSystem.setParameterByName(m_parameterName, m_parameterValue);
+            }
+
+            if(m_triggerEvent)
+            {
+                FMODUnity.RuntimeManager.PlayOneShot(m_eventName);
+            }
+        }
+    }
+}
